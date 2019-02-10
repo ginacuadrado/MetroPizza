6d966eb22b8d029a5d00de592f1448aae25e83f7
Samuel Velasquez <velasquez.samuel@correo.unimet.edu.ve>
2019-02-10 17:50:02 -0400

    Thread extensions and comment formatting

--- src/Employees/WaiterChief.java
+++ src/Employees/WaiterChief.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Employees;
 
-/**
- *
- * @author pc ready
- */
-public class WaiterChief {
+public class WaiterChief extends Thread {
     
 }

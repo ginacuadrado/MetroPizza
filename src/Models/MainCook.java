0be4b612329c82de9592f982244163ae35aa47e9
Samuel Velasquez <velasquez.samuel@correo.unimet.edu.ve>
2019-02-25 15:46:12 -0400

    Fixed Main Cook class

--- src/Models/MainCook.java
+++ src/Models/MainCook.java
@@ -2,6 +2,8 @@
 package Models;
 
 import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MainCook extends Thread {
     
@@ -21,6 +23,7 @@ public MainCook(/*Table table, int time, int in, Semaphore SemME, Semaphore SemM
     this.SemW = SemW;*/
 }
 
+//GETTER AND SETTER FOR COOK STATUS
     public boolean isHire() {
         return hire;
     }
@@ -28,6 +31,31 @@ public MainCook(/*Table table, int time, int in, Semaphore SemME, Semaphore SemM
     public void setHire(boolean hire) {
         this.hire = hire;
     }
-    
- 
+
+//DEFINITION OF THREAD RUN METHOD
+    @Override
+    public void run(){
+       while(this.hire){
+            try{
+                this.SemMC.acquire();
+                
+                //Wait the set amount of time before producing a plate
+                Thread.sleep(this.time);
+                
+                //Making sure to have ME when setting a plate on the table
+                this.SemME.acquire();
+                
+                //Setting a plate on th table
+                this.table.setPlate(Restaurant.inMain, 1);
+                Restaurant.inMain = (Restaurant.inMain + 1) % this.table.getMax();
+                Restaurant.addMain();
+                
+                this.SemW.release();
+                this.SemME.release();
+            }
+         catch(InterruptedException ex) {
+            Logger.getLogger(AppetizerCook.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
 }
